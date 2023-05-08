c0393037a2fab575a12049022753b770b4d02e1c
Stiven <s.valenciar0@gmail.com>
2023-05-08 11:21:05 -0500

    add TODO, main.tf

---
+++ main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "ubuntu_terraform" {
+    ami = "ami-007855ac798b5175e"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "Ubuntu server"
+        Owner = "Devops student"
+        Project = "Terraform intro"
+    }
+}
+
+resource "aws_security_group" "Terraform_SG" {
+  name        = "Security Group using Terraform"
+  description = "Security Group using Terraform"
+  vpc_id      = "vpc-069d5bd758500ab21"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Terraform_SG"
+  }
+}
