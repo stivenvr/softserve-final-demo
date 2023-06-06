f116a34cf369a916ef8ed56b2d7a69a3d50e9a64
stiven valencia <s.valenciar0@gmail.com>
2023-06-06 13:22:16 -0500

    SFD-5  try of code smell

--- algorithm.go
+++ algorithm.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
@@ -27,35 +26,6 @@ func (m Message) Encrypt() string {
 	result = strings.Replace(result, "i", "imes", -1)
 	result = strings.Replace(result, "o", "ober", -1)
 	result = strings.Replace(result, "u", "ufat", -1)
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
-	fmt.Println("HOLA")
 	return result
 
 }
